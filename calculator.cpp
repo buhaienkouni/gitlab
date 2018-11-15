7d4d2a20df8d69cf092e5079c9ab9f4d05411360
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -7,5 +7,17 @@ int Calculator::Add (int a, int b)
 
 int Calculator::Mul (double a, double b)
 {
+
     return a * b + 0.5;
+}
+int Calculator::RandomLogic(int x, int y) {
+    int result = (x * 2) + (y / 3);
+
+    return result;
+}
+
+double Calculator::AnotherRandomLogic(double p, double q) {
+    double result = (p * q) - (p / q) + 42.0;
+
+    return result;
 }
